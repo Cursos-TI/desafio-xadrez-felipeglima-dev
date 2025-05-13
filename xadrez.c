9afa127bd10c676cf273b2c9c09c099796ae9b8d
Felipe Lima <felipelimabox@gmail.com>
2025-05-13 21:04:00 +0000

    Implementação de lógica com estruturas de repetição aninhadas. Tema 3 - Nível aventureiro. Linha 41.

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,66 @@
 #include <stdio.h>
 #include<locale.h>
 
-// Desafio de Xadrez - MateCheck -Tema 3 - Desafio Novato
+// Desafio de Xadrez - MateCheck -Tema 3 - Desafio Aventureiro
 
 int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
 
     // --------- MOVIMENTO DA TORRE ---------
     // Move 5 casas para a direita, usando FOR
+
     printf("Movimento da Torre (5 casas para a Direita):\n");
-    for (int movimento = 1; movimento <= 5; movimento++) {
-        printf("Movendo para Direita\n");
+    for (int torremovimento = 1; torremovimento <= 5; torremovimento++) {
+        printf("Movendo Torre para Direita.\n");
     }
 
     // --------- MOVIMENTO DO BISPO ---------
     // Move 5 casas na diagonal (Cima + Direita), usando WHILE
+
     printf("\nMovimento do Bispo (5 casas na Diagonal para Cima Direita):\n");
-    int bispomovimentos = 0;
-    while (bispomovimentos < 5) {
-        printf("Movendo para Cima Direita\n");
-        bispomovimentos++;
+    int bispomovimento = 0;
+    while (bispomovimento < 5) {
+        printf("Movendo Bispo para cima/direita.\n");
+        bispomovimento++;
     }
 
     // --------- MOVIMENTO DA RAINHA ---------
     // Move 8 casas para a esquerda, usando DO-WHILE
-    printf("\nMovimento da Rainha (8 casas para a Esquerda):\n");
-    int rainhamovimentos = 0;
-    do {
-        printf("Movendo para Esquerda\n");
-        rainhamovimentos++;
-    } while (rainhamovimentos < 8);
 
-    return 0;
+    printf("\nMovimento da Rainha (8 casas para a Esquerda):\n");
+    int rainhamovimento = 0;
+    do {
+        printf("Movendo Rainha para Esquerda.\n");
+        rainhamovimento++;
+    } while (rainhamovimento < 8);
+
+
+    // --------- MOVIMENTO DO CAVALO ---------
+    // Move 2 casas para baixo, 1 casa para esquerda (Movimento em L) usando LOOP ANINHADO FOR:
+
+    printf("\nMovimento do Cavalo (2 casas para baixo, 1 casa para esquerda):\n");
+    for(int cavalomovimento1 = 1; cavalomovimento1 <= 1; cavalomovimento1++){
+        for(int cavalomovimento2 = 1; cavalomovimento2 <= 2; cavalomovimento2++){
+            printf("Movendo Cavalo para baixo. \n");
+        }
+        printf("Movendo Cavalo para esquerda.\n");
+        printf("----------------------------\n");
+   }
+
+    // Move 2 casas para baixo, 1 casa para esquerda (Movimento em L) usando LOOP ANINHADO DO-WHILE:
+    
+    printf("\nMovimento do Cavalo (2 casas para baixo, 1 casa para esquerda):\n");
+    int cavalomovimento1 = 1;
+    do {
+        int cavalomovimento2 = 1;
+            do {
+                printf("Movendo Cavalo para baixo.\n");
+                cavalomovimento2++;
+                    } while (cavalomovimento2 <= 2);
+                        printf("Movendo Cavalo para esquerda.\n");
+                        cavalomovimento1++;
+        } while (cavalomovimento1 <= 1);
+
+return 0;
 }
+
